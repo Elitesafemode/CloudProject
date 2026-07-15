99e87fb0971cf86cfb03b5c8b2343e07e3a7bd98
AlirezaRostamiSoleimani <elitesafemode@gmail.com>
2026-07-15 19:32:04 +0330

    fix evn variable

--- Linkshortener/Program.cs
+++ Linkshortener/Program.cs
@@ -66,7 +66,7 @@ namespace Linkshortener
                 var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
                 db.Database.Migrate();
             }
-
+            app.MapGet("/", () => "OK");
             app.Run();
         }
     }
